927f027d30ce846b9d9517d5db753e817d02d2c5
sag7221 <toshilpa@gmail.com>
2022-10-23 09:13:18 -0700

    Module 7 challenge code

---
+++ Queries/Employee_Database_Challenge.sql
@@ -0,0 +1,32 @@
+SELECT emp_no, first_name, last_name 
+FROM employees;
+
+SELECT title, from_date, to_date
+FROM titles;
+
+SELECT e.emp_no, e.first_name, e.last_name, ti.title, ti.from_date, ti.to_date
+INTO retirement_titles
+FROM employees as e
+LEFT JOIN titles as ti
+ON (e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no;
+
+DROP TABLE retirement_titles;
+
+SELECT * FROM retirement_titles;
+
+--Select only distinct 
+SELECT DISTINCT ON (emp_no) emp_no,
+first_name,
+last_name,
+title
+INTO unique_titles
+FROM retirement_titles
+WHERE to_date = '9999-01-01'
+ORDER BY emp_no ASC, to_date DESC;
+
+SELECT * FROM unique_titles;
+
+DROP TABLE unique_titles;
+
